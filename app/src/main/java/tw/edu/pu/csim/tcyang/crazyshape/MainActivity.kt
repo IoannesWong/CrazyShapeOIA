2b100d52bbbec490248155e4a8d40627c715ff1d
ioannes <o1083020@gm.pu.edu.tw>
2021-05-31 10:55:05 +0800

    2021上機考v3_(黃勤義)

--- app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
@@ -14,19 +14,21 @@ public final class MyAppGlideModule : AppGlideModule()
 
 
 class MainActivity : AppCompatActivity() {
+
+    var Flag:Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Toast.makeText(baseContext, "作者：黃勤義", Toast.LENGTH_LONG).show()
+
         GlideApp.with(this)
             .load(R.drawable.cover)
             .override(800, 600)
             .into(imgTitle)
 
-
-
-
+        RndShape()
 
         imgNext.setOnLongClickListener(object : View.OnLongClickListener {
             override fun onLongClick(p0: View?): Boolean {
@@ -35,5 +37,21 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
         })
+        imgNext.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(v: View?) {
+                RndShape()
+            }
+
+        })
+    }
+
+    fun RndShape() {
+        Flag=(1..4).random()
+        when(Flag){
+            1->imgNext.setImageResource(R.drawable.circle)
+            2->imgNext.setImageResource(R.drawable.square)
+            3->imgNext.setImageResource(R.drawable.star)
+            4->imgNext.setImageResource(R.drawable.triangle)
+        }
     }
 }
