bca2a80380c242e637cd56c87e81ef571e8e46c2
ioannes <o1083020@gm.pu.edu.tw>
2021-05-31 10:40:57 +0800

    2021上機考v2.5_(黃勤義)

--- app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
@@ -26,6 +26,8 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
+
         imgNext.setOnLongClickListener(object : View.OnLongClickListener {
             override fun onLongClick(p0: View?): Boolean {
                 intent = Intent(this@MainActivity, GameActivity::class.java)
